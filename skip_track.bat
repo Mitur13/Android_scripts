61a19d2a115e07d1462870aeaab703ce99949d2a
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-14 16:09:05 +0100

    Track changes countr

--- skip_track.bat
+++ skip_track.bat
@@ -1,12 +1,13 @@
-@echo off 
+@echo off
 adb shell input keyevent KEYCODE_MEDIA_PLAY
 echo Playing Media
 timeout /t 10
 
-for /l %%x in (1, 1, 24) do (
-   adb shell input keyevent KEYCODE_MEDIA_NEXT
-   echo Playing Next Track
-   timeout /t 5
+for /l %%x in (1, 1, 120) do (
+	adb shell input keyevent KEYCODE_MEDIA_NEXT
+	echo Playing Next Track
+	timeout /t 10
+	echo ######  %%x  ######
 )
 
 adb shell input keyevent KEYCODE_MEDIA_PAUSE
