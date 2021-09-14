7f1a383fbc51b55162c9555ca1153559f5db3dc5
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-14 12:09:49 +0100

    Add files via upload

---
+++ skip_track.bat
@@ -0,0 +1,12 @@
+@echo off 
+adb shell input keyevent KEYCODE_MEDIA_PLAY
+echo Playing Media
+timeout /t 10
+
+for /l %%x in (1, 1, 5) do (
+   adb shell input keyevent KEYCODE_MEDIA_NEXT
+   echo Playing Next Track
+   timeout /t 10
+)
+
+adb shell input keyevent KEYCODE_MEDIA_PAUSE
