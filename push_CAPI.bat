2aea8b5479806b79fbe165d9744650b72aa88ff0
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-14 13:35:00 +0100

    Add files via upload

---
+++ push_CAPI.bat
@@ -0,0 +1,20 @@
+@echo off
+adb root
+adb remount
+
+echo Paste CAPI folder directory:
+set /p CapiPath=
+cd %CapiPath%
+
+adb shell mount -o rw,remount /vendor/dsp
+
+@echo on
+adb push aptx_adaptive_enc_module.so.1 /vendor/dsp/adsp
+adb push libaptXAdaptiveEnc.so /vendor/dsp/adsp
+adb push libaptXAdaptiveEnc3.so /vendor/dsp/adsp
+
+@echo off
+
+echo Press any key to restart the phone...
+pause > nul
+adb reboot
