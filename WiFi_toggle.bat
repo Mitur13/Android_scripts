4914da86be0387ca0842dfecf4828ab73c26037f
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-14 16:10:58 +0100

    Cycle counter

--- WiFi_toggle.bat
+++ WiFi_toggle.bat
@@ -1,10 +1,11 @@
 @echo off 
 for /l %%x in (1, 1, 90000) do (
    adb shell svc wifi disable
-   echo WLAN Disabled
+   echo        WLAN Disabled
    timeout /t 10
    adb shell svc wifi enable
-   echo 		WLAN Enabled
+   echo 		     WLAN Enabled
    timeout /t 20
+   echo Cycle #%%x
 )
 adb shell svc wifi disable
