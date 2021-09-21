5160ad9a604cadc8f574e92b64d2f20dd19d2d33
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-21 11:08:06 +0100

    Add files via upload

--- pause_wait_skip_play.bat
+++ pause_wait_skip_play.bat
@@ -1,9 +1,10 @@
 @echo off
 
 adb shell input keyevent KEYCODE_MEDIA_PLAY
+timeout /t 30
 
 for /l %%x in (1, 1, 25) do (
-	echo ######  %%x  ######
+	echo 		#####  %%x  #####
 	adb shell input keyevent KEYCODE_MEDIA_PAUSE
 	timeout /t 5
 
