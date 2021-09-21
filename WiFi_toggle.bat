44f035907d8af9f35b4603e5c7ff1b2704ce5cc1
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-21 10:20:21 +0100

    Add files via upload
    Moved the counter at the top

--- WiFi_toggle.bat
+++ WiFi_toggle.bat
@@ -1,11 +1,11 @@
 @echo off 
 for /l %%x in (1, 1, 90000) do (
+   echo #####  %%x  #####
    adb shell svc wifi disable
    echo        WLAN Disabled
    timeout /t 10
    adb shell svc wifi enable
    echo 		     WLAN Enabled
    timeout /t 20
-   echo Cycle #%%x
 )
 adb shell svc wifi disable
