9e9a1f0f43b01522910e005f870ce502e2ad7399
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-09-21 10:21:26 +0100

    Wait, skip, play

---
+++ pause_wait_skip_play.bat
@@ -0,0 +1,15 @@
+@echo off
+
+adb shell input keyevent KEYCODE_MEDIA_PLAY
+
+for /l %%x in (1, 1, 25) do (
+	echo ######  %%x  ######
+	adb shell input keyevent KEYCODE_MEDIA_PAUSE
+	timeout /t 5
+
+	adb shell input keyevent KEYCODE_MEDIA_NEXT
+	timeout /t 30
+)
+echo finished
+adb shell input keyevent KEYCODE_MEDIA_PAUSE
+pause > null
