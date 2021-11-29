68c6509d652bb67794e9767a7edca4a73550d547
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-11-29 14:43:51 +0000

    apk install and cleanup
    Cleaned up the commands and added apk installation

--- phone_setup.bat
+++ phone_setup.bat
@@ -4,18 +4,19 @@ adb wait-for-device remount
 adb wait-for-device shell mount -o rw,remount /vendor/dsp
 
 adb shell setprop persist.vendor.qcom.bluetooth.aptxadaptiver2_2_support true
-cd C:\audio_files\1.2kHz
-
-@echo on
+echo Aptx Adaptive R2_2 support is:
 adb shell getprop persist.vendor.qcom.bluetooth.aptxadaptiver2_2_support
 
 
-adb wait-for-device push 10min /sdcard/Music/
-adb wait-for-device push 30min /sdcard/Music/
+adb wait-for-device push C:\audio_files\1.2kHz\10min /sdcard/Music/
+adb wait-for-device push C:\audio_files\1.2kHz\30min /sdcard/Music/
+
+
+adb install C:/tools/googlesynth.apk
+adb install C:/tools/nRF_Connect_v4.24.3.apk
+
 adb shell sync
 
-@echo off
 echo Press any key to restart the phone
 pause > null
 adb reboot
-pause > null
