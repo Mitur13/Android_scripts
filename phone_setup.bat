ce26a5a1d999100860eea3fb21f772d5482c1e17
Mateusz Miturski <90606813+Mitur13@users.noreply.github.com>
2021-11-30 16:51:24 +0000

    Dev options, system sounds, screen
    Added enabling developer options, setting screen timeout to 10 mins, disabling system sounds

--- phone_setup.bat
+++ phone_setup.bat
@@ -15,8 +15,22 @@ adb wait-for-device push C:\audio_files\1.2kHz\30min /sdcard/Music/
 adb install C:/tools/googlesynth.apk
 adb install C:/tools/nRF_Connect_v4.24.3.apk
 
+adb shell settings put global development_settings_enabled 1
+adb shell settings put secure bluetooth_hci_log 1
+
+adb wait-for-device shell svc bluetooth disable
+adb wait-for-device shell svc bluetooth enable
+
+adb shell settings put system screen_off_timeout 600000
+
+adb shell settings put system lockscreen_sounds_enabled 0
+adb shell settings put system dtmf_tone 0
+adb shell settings put system haptic_feedback_enabled 0
+adb shell settings put system sound_effects_enabled 0
+
 adb shell sync
 
-echo Press any key to restart the phone
+
+echo Setup done, press any key to restart the phone
 pause > null
 adb reboot
